e4a912653981fc1c0c96fcdf408ee66d4a91f54e
si2dharth <si2dharth2@gmail.com>
2015-06-30 11:48:04 -0400

    Added stdafx.h

---
+++ BigKinectServer/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+#include <d2d1.h>
+#include <Kinect.h>
+#include <string>
+#pragma comment(lib, "d2d1.lib")
+
+using namespace std;
